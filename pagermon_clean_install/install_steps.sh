0af699a4b6f14270f878c1fccea6eb6332eef3da
bullseye555 <nabro0813@gmail.com>
2024-03-23 11:17:28 +1100

    Update from Bongo feedback

--- pagermon_clean_install/install_steps.sh
+++ pagermon_clean_install/install_steps.sh
@@ -26,11 +26,33 @@ cd ~/pagermon/server
 sudo npm install -g node-pre-gyp
 sudo npm install sqlite3@5.0.0
 
-#### Delete the node_modules folder and copy in the contents of node_modules_SERVER.tar.gz (after extracting from the archive)
-#### Be sure to update process.json and any other customisations now
+#### Whilst in /pagermon/server directory delete the node_modules folder if present and copy in the contents of node_modules_SERVER.tar.gz (after extracting from the archive)
+#### Be sure to update any other customisations now
+
+#### Next we'll need to create and edit our Process.json file.
+#### Create a copy of the default file
+
+cp $HOME/pagermon/server/process-default.json $HOME/pagermon/server/process.json
+
+#### Open the file using your favourite editor vim/nano etc, for the purposes of this guide we will use nano.
+
+nano $HOME/pagermon/server/process.json
+
+#### Edit line 3 to match your environment.
+
+"cwd"              : "/home/$USER/pagermon/server",
+
+#### Edit Line 22 to your domain name (if desired)
+
+"HOSTNAME": "pagermonhome.local",
+
+Save the file by using the hotkey CTRL-O, close the file by pressing CTRL-X.
+
 
 sudo npm install -e NODE_OPTIONS='--max-old-space-size=2048'
 
+#### This NPM Audit Fix line is OPTIONAL. If you do a clean install and find dependency errors, repeat this clean-install process but do NOT complete this step
+#### You may need to do a clean install of Linux (or do some Google-fu to find out how to remove the installed nodes)
 sudo npm audit fix
 
 export NODE_ENV=production
@@ -81,12 +103,8 @@ sudo make install
 
 cd ~/pagermon/client
 
-#### Delete the node_modules folder and copy in the contents of node_modules_CLIENT.tar.gz (after extracting from the archive)
+#### Whilst in /pagermon/client directory Delete the node_modules folder if present and copy in the contents of node_modules_CLIENT.tar.gz (after extracting from the archive)
 #### Be sure to update reader.sh, config/* and any other customisations now
-
-npm install -e NODE_OPTIONS='--max-old-space-size=2048'reader.sh
-
-sudo npm audit fix 
 
 ## Start Pagermon Server [Web Host]
 
@@ -98,9 +116,28 @@ pm2 save
 
 sudo pm2 startup
 
-## Start Pagermon Client [Decoding]
+#### Before running Pagermon Client you have to configure it to send the decoded info to the pagermon server.
+#### Copy default.json to config.json
 
 cd ~/pagermon/client/
+
+cp config/default.json config/config.json 
+
+#### Edit config.json with your favorite editor.
+#### Update the default API by accessing the server, eg: 192.168.1.10:3000, (Your IP may differ). Navigate to Settings and find then delete the fake APIs and create new APIs
+#### Copy the new API key to config.json and make any other updates accordingly
+#### Update your reader.sh file accordingly and save 
+
+npm install -e NODE_OPTIONS='--max-old-space-size=2048'reader.sh
+
+#### This NPM Audit Fix line is OPTIONAL. If you do a clean install and find dependency errors, repeat this clean-install process but do NOT complete this step
+#### You may need to do a clean install of Linux (or do some Google-fu to find out how to remove the installed nodes)
+sudo npm audit fix 
+
+## Start Pagermon Client [Decoding]
+
 pm2 start reader.sh
+
 pm2 save
+
 pm2 startup
